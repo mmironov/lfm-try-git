58ec1948f52819c4090dcc881053fe6eac6d8768
Miroslav <miroslavmironov@gmail.com>
2017-08-27 15:19:01 +0300

    Changed the message to display.

--- src/com/musala/lfm/Program.java
+++ src/com/musala/lfm/Program.java
@@ -3,14 +3,10 @@ package com.musala.lfm;
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("A change in the program");
+		System.out.println("Tralala");
 		print();
 	}
 	private static void print() {
 		System.out.println("This is a printed message");
-	}
-	
-	private void sayHello() {
-		
 	}
 }
