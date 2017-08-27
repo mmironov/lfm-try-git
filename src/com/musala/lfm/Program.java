66edad322939753d84eafa6c8a38e8ab585d1375
Denis Hristov <dhristov99626@gmail.com>
2017-08-27 15:12:42 +0300

    Added a Print function to the Program.java file.

--- src/com/musala/lfm/Program.java
+++ src/com/musala/lfm/Program.java
@@ -4,5 +4,9 @@ public class Program {
 
 	public static void main(String[] args) {
 		System.out.println("A change in the program");
+		print();
+	}
+	private static void print() {
+		System.out.println("This is a printed message");
 	}
 }
